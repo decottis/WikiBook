86e918e5b9eb28b38fa4cc12326b4aadfea329c9
soulk <fitoussi.david60@gmail.com>
2015-10-07 00:30:05 +0200

    Ajout ActionBar

--- app/src/main/java/com/example/s/wikibook/CollectionActivity.java
+++ app/src/main/java/com/example/s/wikibook/CollectionActivity.java
@@ -2,8 +2,13 @@ package com.example.s.wikibook;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
@@ -12,8 +17,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class CollectionActivity extends Activity {
-
+public class CollectionActivity extends ActionBarActivity {
+    int lastItemClicked = -1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,15 +50,30 @@ public class CollectionActivity extends Activity {
                 new int[] {R.id.img_cover, R.id.author, R.id.title, R.id.isbn, R.id.year, R.id.description});
 
         bookList.setAdapter(listAdapter);
+        bookList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position,
+                                    long id) {
+                ActionBar actionBar = getSupportActionBar();
+                actionBar.show();
+                HashMap<String,String> map = (HashMap<String,String>)parent.getItemAtPosition(position);
+                actionBar.setTitle(map.get("title"));
+                lastItemClicked = position;
+                System.out.println(position +"   " + id);
+            }
+        });
     }
-
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        // Inflate the menu items for use in the action bar
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_action_bar_book, menu);
+
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
