99140811e1ef23c4cda0f6853076c043a1d3897a
soulk <fitoussi.david60@gmail.com>
2015-10-07 00:32:55 +0200

    Ajout ActionBar

---
+++ app/src/main/java/com/example/s/wikibook/ActionBarBook.java
@@ -0,0 +1,20 @@
+package com.example.s.wikibook;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class ActionBarBook extends ActionBarActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_action_bar_book, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
